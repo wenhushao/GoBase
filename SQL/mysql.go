a5cfe6a7b44043769c0a923396ac945540033562
shaowenhu <swhu0330@163.com>
2020-07-05 10:37:53 +0800

    add sql

---
+++ SQL/mysql.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var (
+	userName  string = "root"
+	password  string = "201606"
+	ipAddrees string = "127.0.0.1"
+	port      int    = 3306
+	dbName    string = "test"
+	charset   string = "utf8"
+)
+
+//连接数据库
+func connectMysql() *sqlx.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s", userName, password, ipAddrees, port, dbName, charset)
+	Db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		fmt.Printf("mysql connect failed, detail is [%v]", err.Error())
+	}
+	return Db
+}
+
+//添加数据
+func addRecord(Db *sqlx.DB) {
+	for i := 0; i < 2; i++ {
+		result, err := Db.Exec("insert into user  values(?,?,?)", 2, "johny", "123456")
+		if err != nil {
+			fmt.Printf("data insert faied, error:[%v]", err.Error())
+			return
+		}
+		id, _ := result.LastInsertId()
+		fmt.Printf("insert success, last id:[%d]\n", id)
+	}
+}
+
+func updateRecord(Db *sqlx.DB) {
+	//更新uid=1的username
+	result, err := Db.Exec("update user set username = 'anson' where userid = 1")
+	if err != nil {
+		fmt.Printf("update faied, error:[%v]", err.Error())
+		return
+	}
+	num, _ := result.RowsAffected()
+	fmt.Printf("update success, affected rows:[%d]\n", num)
+}
+
+func deleteRecord(Db *sqlx.DB) {
+	//删除uid=2的数据
+	result, err := Db.Exec("delete from user where userid = 2")
+	if err != nil {
+		fmt.Printf("delete faied, error:[%v]", err.Error())
+		return
+	}
+	num, _ := result.RowsAffected()
+	fmt.Printf("delete success, affected rows:[%d]\n", num)
+}
+func queryData(Db *sqlx.DB) {
+	rows, err := Db.Query("select * from user")
+	if err != nil {
+		fmt.Printf("query faied, error:[%v]", err.Error())
+		return
+	}
+	for rows.Next() {
+		//定义变量接收查询数据
+		var userid int
+		var username, password string
+
+		err := rows.Scan(&userid, &username, &password)
+		if err != nil {
+			fmt.Println("get data failed, error:[%v]", err.Error())
+		}
+		fmt.Println(userid, username, password)
+	}
+
+	//关闭结果集（释放连接）go
+	rows.Close()
+}
+
+func main() {
+	var Db *sqlx.DB = connectMysql()
+	defer Db.Close()
+	//addRecord(Db)
+	//updateRecord(Db)
+	//deleteRecord(Db)
+	queryData(Db)
+}
