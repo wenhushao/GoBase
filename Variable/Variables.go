54f2a421c994bb08bf188c0283a7e1c69e6bb538
shaowenhu <swhu0330@163.com>
2020-07-18 15:35:10 +0800

    Initial commit

--- Variable/Variables.go
+++ Variable/Variables.go
@@ -3,13 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	//变量
+	x := 10
+	println("x=", x)
+	//fmt.Println("x=",x)
+
 	var a = "hello"
 	//fmt.Println("a="+a)
-	fmt.Println("a=",a)
+	fmt.Println("a=", a)
 
 	var b int
-	fmt.Println("b=",b)
+	fmt.Println("b=", b)
 
 	var c bool
-	fmt.Println("c=",c)
+	fmt.Println("c=", c)
 }
