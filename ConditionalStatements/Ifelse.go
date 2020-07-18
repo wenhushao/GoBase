9227a3a0ddf261ad9025b395905196f11a4b2a61
shaowenhu <swhu0330@163.com>
2020-07-18 16:58:58 +0800

    Initial commit

--- ConditionalStatements/Ifelse.go
+++ ConditionalStatements/Ifelse.go
@@ -1,12 +1,21 @@
 package main
 
 func main() {
-	var a=0
-	if a>0{
+	var a = 0
+	if a > 0 {
 		println("a>0")
-	}else if a==0 {
+	} else if a == 0 {
 		println("a=0")
-	}else {
+	} else {
 		println("a<0")
+	}
+
+	b := 100
+	if b > 100 {
+		println("b>100")
+	} else if b == 100 {
+		println("b=100")
+	} else {
+		println("b<100")
 	}
 }
--- Variable/Variables.go
+++ Variable/Variables.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func main() {
 	//变量
-	//var 变量名字 类型 = 表达式
+	//var 变量名字 类型 = 表达式；变量名:=表达式
 
 	//var x int32
 	//x = 10
 	x := 10
 	println("x=", x)
 	//fmt.Println("x=",x)
+
+	var s string
+	s = "string01"
+	println("s=", s)
+
+	s1 := "string02"
+	println("s1=", s1)
 
 	var a = "hello"
 	//fmt.Println("a="+a)
