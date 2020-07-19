6f389c490e53a3580e03b9b52ce9dddf822d1717
shaowenhu <swhu0330@163.com>
2020-07-19 14:39:41 +0800

    Initial commit

--- ConditionalStatements/Switch.go
+++ ConditionalStatements/Switch.go
@@ -3,20 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	//switch
-	var b = 1
-	switch b {
-	case 1:
-		fmt.Print("b=1")
-		break
-	case 2:
-		fmt.Print("b=2")
-		break
-	case 3:
-		fmt.Print("b=3")
-		break
+	/* 定义局部变量 */
+	var grade string = "B"
+	var marks int = 90
+
+	switch marks {
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C"
 	default:
-		fmt.Print(b)
-		break
+		grade = "D"
 	}
+
+	switch {
+	case grade == "A":
+		fmt.Printf("优秀!\n")
+	case grade == "B", grade == "C":
+		fmt.Printf("良好\n")
+	case grade == "D":
+		fmt.Printf("及格\n")
+	case grade == "F":
+		fmt.Printf("不及格\n")
+	default:
+		fmt.Printf("差\n")
+	}
+	fmt.Printf("你的等级是 %s\n", grade)
 }
