a3b3566b60796d7013a0b9aebec8a29d281ab1db
shaowenhu <swhu0330@163.com>
2020-07-21 18:02:52 +0800

    Initial commit

--- KeyboardEntry/Scan1.go
+++ KeyboardEntry/Scan1.go
@@ -9,14 +9,8 @@ func main() {
 	var sal float32
 	var isPass bool
 
-	fmt.Println("请输入姓名：")
-	fmt.Scanln(&name)
-	fmt.Println("请输入年龄：")
-	fmt.Scanln(&age)
-	fmt.Println("请输入薪水：")
-	fmt.Scanln(&sal)
-	fmt.Println("请输入是否通过考试：")
-	fmt.Scanln(&isPass)
+	fmt.Println("请输入姓名， 年龄， 薪水 是否通过考试， 使用空格隔开")
+	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
 
 	fmt.Printf("名字：%v \n年龄：%v \n薪水：%v \n是否通过考试：%v \n", name, age, sal, isPass)
 }
