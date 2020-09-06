10f161ec3ce71eb70a4500851b1fd2f6809d3f7e
shaowenhu <swhu0330@163.com>
2020-09-06 09:39:56 +0800

    Initial commit

--- Array/Array1.go
+++ Array/Array1.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 	//数组
 	numbers := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println("numbers数组为：")
+	fmt.Println("Numbers数组为：")
 	for i := 0; i < len(numbers); i++ {
 		fmt.Print(numbers[i], " ")
 	}
 	fmt.Println()
-	fmt.Println("strings数组为：")
+	fmt.Println("Strings数组为：")
 	strings := []string{"Apple", "Google", "Microsoft"}
 	for i := 0; i < len(strings); i++ {
 		fmt.Print(strings[i], " ")
